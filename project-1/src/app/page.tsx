87372d0492c721d998a47609107dfe985dd5f7c7
SheryarAmir <dev.sheryaramir@gmail.com>
2025-10-03 14:08:23 +0500

    working with supabase

---
+++ project-1/src/app/page.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const page = () => {
+  return (
+    <div>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio vero,
+      obcaecati sit aliquid nisi commodi placeat dicta aperiam autem iusto
+      dolore nulla labore suscipit delectus totam aut asperiores maxime fuga?
+    </div>
+  );
+};
+
+export default page;
