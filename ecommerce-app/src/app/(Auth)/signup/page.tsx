a2dfe09df10fe79127e97c7c01e8b29e74657f14
SheryarAmir <dev.sheryaramir@gmail.com>
2025-12-16 19:51:03 +0500

    lastest changes

--- ecommerce-app/src/app/(Auth)/signup/page.tsx
+++ ecommerce-app/src/app/(Auth)/signup/page.tsx
@@ -95,7 +95,6 @@ export default function SignUp() {
     }
   };
 
-
   return (
     <div className="min-h-screen flex items-center justify-center bg-gray-50 py-12 px-4 sm:px-6 lg:px-8">
       <div className="max-w-md w-full space-y-8">
